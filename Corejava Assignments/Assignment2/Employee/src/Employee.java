446d632ae4c10e669c76bf1f3667ae82c93e2f01
Senthamizhan <42460436+Senthamiz13@users.noreply.github.com>
2021-07-09 20:27:28 +0530

    assignment

---
+++ Corejava Assignments/Assignment2/Employee/src/Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+    String name;
+    String desig;
+    int Salary;
+
+    public Employee(String name, String desig, int salary) {
+        this.name = name;
+        this.desig = desig;
+        this.Salary = salary;
+    }
+
+    int totalsalary(){
+        return 0;
+    }
+
+}
