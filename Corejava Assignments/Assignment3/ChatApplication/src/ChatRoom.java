73af91e88b3ee93775634b0f2aae6cfdf146b97a
Senthamizhan <42460436+Senthamiz13@users.noreply.github.com>
2021-07-14 21:48:24 +0530

    new chat room

--- Corejava Assignments/Assignment3/ChatApplication/src/ChatRoom.java
+++ Corejava Assignments/Assignment3/ChatApplication/src/ChatRoom.java
@@ -1,12 +1,7 @@
-import java.util.ArrayList;
-import java.util.HashSet;
 class ChatRoom{
 
     String name;
     public ChatRoom(String name) {
         this.name = name;
     }
-    ArrayList<User> users=new ArrayList<>();    ///hashsetusers
-    ArrayList<Message> messages=new ArrayList<Message>();  //Arraylist messages
-
 }
